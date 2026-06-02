cce49a9ce5991d70b24b60f44e47ef8de1c159ea
mahawarkhushi <mahawarkhushi27@gmail.com>
2026-06-02 11:42:31 +0530

    made edit working

--- src/manifest.tsx
+++ src/manifest.tsx
@@ -1,6 +1,7 @@
 import { lazy } from "react";
 import EAusdhadhiFetchPage from "./pages/EAusdhadhiFetchPage.tsx";
 import DeliveryOrderShow from "./pages/DeliveryOrderShow.tsx";
+import DeliveryOrderForm from "./pages/DeliveryOrderForm.tsx";
 
 const manifest = {
   plugin: "care_eaushadhi",
@@ -24,6 +25,22 @@ const manifest = {
       />
     ),
 
+    "/facility/:facilityId/locations/:locationId/eaushadhi/:deliveryOrderId/edit": ({
+      facilityId,
+      locationId,
+      deliveryOrderId,
+    }: {
+      facilityId: string;
+      locationId: string;
+      deliveryOrderId: string;
+    }) => (
+      <DeliveryOrderForm
+        facilityId={facilityId}
+        locationId={locationId}
+        deliveryOrderId={deliveryOrderId}
+      />
+    ),
+
     "/facility/:facilityId/locations/:locationId/eaushadhi/:deliveryOrderId": ({
       facilityId,
       locationId,
@@ -36,8 +53,7 @@ const manifest = {
       <DeliveryOrderShow
         facilityId={facilityId}
         locationId={locationId}
-        deliveryOrderId={deliveryOrderId}
-      />
+        deliveryOrderId={deliveryOrderId} internal={false}      />
     ),
   },
 
