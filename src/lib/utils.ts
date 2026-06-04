4f051623db42b817f6dfc9b13ad3e6a108629ff4
Jagankumar E <53823168+jagankumar-egov@users.noreply.github.com>
2026-06-04 16:35:09 +0530

    feat/print linked to exiting feature (#40)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,17 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { navigate } from "raviger";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function navigateToDeliveryPrint(
+  facilityId: string,
+  locationId: string,
+  deliveryId: string
+) {
+  navigate(
+    `/facility/${facilityId}/locations/${locationId}/inventory/external/deliveries/incoming/${deliveryId}/print`
+  );
 }
