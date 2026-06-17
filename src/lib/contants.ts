af133f244cc1b0cafccb209da7ddf7a78c9a0eaf
Jagankumar E <53823168+jagankumar-egov@users.noreply.github.com>
2026-06-17 08:07:59 +0530

    Updated the default timeout & intervals to avoid frequent failures (#94)

--- src/lib/contants.ts
+++ src/lib/contants.ts
@@ -1,3 +1,3 @@
 export const I18NNAMESPACE = "care_eaushadhi";
-export const POLL_INTERVAL_MS = 3_000;
-export const POLLING_TIMEOUT = 10_000;
+export const POLL_INTERVAL_MS = 4_000;
+export const POLLING_TIMEOUT = 16_000;
